1c4bcbc761c65094fe152cf333c267a7c5af33ce
ARUN200204 <132205440+ARUN200204@users.noreply.github.com>
2023-05-01 01:17:53 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+#train dataset
+#i have written the down question and answer using thw original wikipedia website and 
+dataset type = [{question:answer}]
+dataset=[{when did india get independence:india achieved independence on 15 august 1947}
+         { who traded with india:phoenicians, greeks, egyptians, romans, chinese}
+          {Who was the first European to reach the Malabar Coast: portugeese reach malabar coast}
+          {tell me about ezhimala kingdom:ezhimala kingdom succeeded mushika dynasty in 7th century}
+          {whendid cholas  defeated pandyas: Chola defeated pandyan empire in 13th century}
+          ]
+# i have above questions and passed it to the the nlp code and got the following answers
+
+dataset=[{when did india get independence:india achieved independence on 15 august 1947}
+         { who traded with india:phoenicians}
+          {Who was the first European to reach the Malabar Coast: portugeese  }
+          {tell me about ezhimala kingdom:ezhimala kingdom succeeded mushika dynasty in 7th century}
+          {whendid cholas  defeated pandyas: Chola defeated pandyan empire in 13th century}
+          ]
