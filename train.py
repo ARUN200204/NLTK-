75f48bd3aaf91adaebbcd19ca3295f0772580d97
ARUN200204 <132205440+ARUN200204@users.noreply.github.com>
2023-04-30 23:52:16 +0530

    train.py

---
+++ train.py
@@ -0,0 +1,8 @@
+#train dataset
+dataset type = [{question:answer}]
+dataset=[{when did india get independence:india achieved independence on 15 august 1947}
+         { who traded with india:phoenicians, greeks, egyptians, romans, chinese}
+          {Who was the first European to reach the Malabar Coast: portugeese reach malabar coast}
+          {tell me about ezhimala kingdom:ezhimala kingdom succeeded mushika dynasty in 7th century}
+          {whendid cholas  defeated pandyas: Chola defeated pandyan empire in 13th century}
+          ]
